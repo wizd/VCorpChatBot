db992dfe476e59390578fa69cb560e1bc394b5d9
wizard <wizard@DESKTOP-2LOR8VV>
2023-04-12 11:36:36 -0700

    first change

---
+++ src/gptTurboApi.ts
@@ -0,0 +1,124 @@
+const dotnev = require("dotenv");
+dotnev.config();
+import { fetchApi } from "./utils";
+
+const chatGPTUrl = "http://192.168.3.59:4004/v1/chat/completions";
+
+const chatWithGPT = async (messages: any[]) => {
+  const headers: Record<string, any> = {
+    Authorization: `Bearer thei5CeseiKosh3huR4nuKeLOhg6ohv1Hol2owuL`,// ${process.env.OPEN_AI_KEY}`,
+  };
+  let apiUrl = chatGPTUrl
+  let { CHATY_PROXY } = process.env
+  // if (CHATY_PROXY) {
+  //   if (CHATY_PROXY[CHATY_PROXY.length - 1] !== '/') {
+  //     CHATY_PROXY += '/'
+  //   }
+  //   headers.origin = 'https://app.uniswap.org'
+  //   apiUrl = CHATY_PROXY + chatGPTUrl
+  // }
+  const answer = await fetchApi(
+    apiUrl,
+    "POST",
+    { headers, timeout: 60000 },
+    {
+      model: "gpt-3.5-turbo",
+      messages,
+    }
+  );
+  return answer;
+};
+
+export const messageManager = (() => {
+  let messageMap: Record<string, any[]> = {};
+  let usageList: Record<string, any[]> = {};
+  return {
+    addUsage: (usage: any, user: string) => {
+      if (!usageList[user]) {
+        usageList[user] = []
+      }
+      usageList[user].push(usage);
+    },
+    getUsage: (user: string) => {
+      if (usageList[user]) {
+        return usageList[user]
+      }
+    },
+    getUsagePrint: (user: string) => {
+      const allUsage = usageList[user]
+      if (!(allUsage && allUsage.length)) return
+      const usage = allUsage[allUsage.length - 1]
+      let ret = '没有额度信息'
+      if (usage) {
+        ret = ''
+        for (let prop in usage) {
+          switch (prop) {
+            case 'prompt_tokens': ret += `您的输入：${usage.prompt_tokens}\n`; break
+            case 'completion_tokens': ret += `回答已用：${usage.completion_tokens}\n`; break
+            case 'total_tokens': ret += `共计：${usage.total_tokens}\n`; break
+          }
+        }
+      }
+      return ret
+    },
+    sendMessage: (content: string, user: string) => {
+      if (!messageMap[user]) {
+        messageMap[user] = [];
+      }
+      messageMap[user].push({ role: "user", content });
+    },
+    concatAnswer: (content: string, user: string) => {
+      if (!messageMap[user]) {
+        messageMap[user] = [];
+      }
+      messageMap[user].push({ role: "assistant", content });
+    },
+    getMessages: (user: string) => {
+      return messageMap[user];
+    },
+    shiftMessage: (user: string) => {
+      messageMap[user].shift();
+    },
+    popMessage: (user: string) => {
+      messageMap[user].pop();
+    },
+    clearMessage: (user: string) => {
+      delete messageMap[user];
+    },
+  };
+})();
+
+export async function resetMessage(user: string) {
+  messageManager.clearMessage(user);
+}
+export async function sendMessage(message: string, user: string) {
+  try {
+    messageManager.sendMessage(message, user);
+    const messages = messageManager.getMessages(user);
+    console.log("-----------newMessages----------");
+    console.log(messages);
+    console.log("-----------newMessages----------");
+    const completion = await chatWithGPT(messages);
+    const answer = completion.choices[0].message.content;
+
+    console.log("-----------newAnswers----------");
+    console.log(answer);
+    console.log("-----------newAnswers----------");
+    messageManager.concatAnswer(answer, user);
+    messageManager.addUsage(completion.usage, user);
+    return answer;
+  } catch (err) {
+    messageManager.popMessage(user);
+    console.log((err as Error).message);
+    let errorBody = (err as Error & { response: any })?.response?.data;
+    console.log(errorBody);
+    let append = "[errored]";
+    try {
+      if (errorBody.error.code === "context_length_exceeded") {
+        append = "[errored][context_length_exceeded]";
+      }
+      errorBody = JSON.stringify(errorBody);
+    } catch (_) { }
+    return (err as Error).message + "   " + errorBody + "[errored]";
+  }
+}
---
+++ src/listeners.ts
@@ -0,0 +1,157 @@
+import {
+  ContactInterface,
+  ContactSelfInterface,
+  FriendshipInterface,
+  MessageInterface,
+  WechatyInterface,
+} from "wechaty/impls";
+import { asyncSleep } from "./utils";
+import { sendMessage, resetMessage, messageManager } from "./gptTurboApi";
+
+function onScan(qrcode: string, status: number) {
+  require("qrcode-terminal").generate(qrcode, { small: true }); // 在console端显示二维码
+  const qrcodeImageUrl = [
+    "https://wechaty.js.org/qrcode/",
+    encodeURIComponent(qrcode),
+  ].join("");
+  console.log(qrcodeImageUrl);
+}
+async function onLogin(user: ContactSelfInterface) {
+  console.log(`User ${user} logged in`);
+}
+function onLogout(user: ContactSelfInterface) {
+  console.log(`${user} 已经登出`);
+}
+async function onFriendship(
+  friendship: FriendshipInterface,
+  bot: WechatyInterface
+) {
+  try {
+    console.log(`received friend event.`);
+    switch (friendship.type()) {
+      case bot.Friendship.Type.Receive:
+        await friendship.accept();
+        break;
+      case bot.Friendship.Type.Confirm:
+        console.log(`friend ship confirmed`);
+        break;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+let gptUserList: ContactInterface[] = [];
+async function onMessage(message: MessageInterface, bot: WechatyInterface) {
+  const contact = message.talker();
+  const room = message.room();
+  let text = message.text();
+  if (room) {
+    try {
+      const topic = await room.topic()
+      const selfName = bot.currentUser.name()
+      console.log(`room topic is : ${topic}, ${text}`)
+      if (text.indexOf(`@${selfName}`) !== -1) {
+        text = text.split(`@${selfName}`)[1].trim()
+        if (!text) return
+        const username = `${topic.toString()}-${contact.toString()}`
+        if (/^(usage|额度|用量)/gim.test(text)) {
+          const humanUsage = await messageManager.getUsagePrint(username);
+          console.log(humanUsage)
+          await message.say(humanUsage!);
+          return
+        }
+        let reply = await sendMessage(text, username);
+        if (/\[errored\]$/gim.test(reply)) {
+          reply = "遇到问题了，请稍后再试！";
+        }
+        if (/\[context_length_exceeded\]$/gim.test(reply)) {
+          reply = "本轮会话长度太长啦，我记不住这么多东西，请重试！";
+        }
+        console.log(reply);
+        room.say(reply, contact)
+      }
+    } catch (err) {
+      console.log((err as Error).message);
+    }
+    return
+  }
+  if (message.self()) return;
+  text = text.trim();
+  console.log(
+    `[${new Date().toLocaleString()}] contact: ${contact}, text:${text}, room: ${room}`
+  );
+  
+  if (!gptUserList.includes(contact)) {
+    gptUserList.push(contact);
+  }
+
+  if (/(你|您)好$/gim.test(text) || /hello/gim.test(text)) {
+    if (!gptUserList.includes(contact)) {
+      message.say(
+        `
+欢迎使用Chaty-基于chatGPT的AI助手~
+您可以输入:
+开始|start: 进入对话
+重置|reset: 重置对话(开始一段新对话)
+退出|exit : 退出对话
+祝您旅途愉快！
+`
+      );
+      return;
+    }
+  }
+  if (/^(开始|start)/gim.test(text)) {
+    if (!gptUserList.includes(contact)) {
+      gptUserList.push(contact);
+      text = "你好";
+    }
+  }
+  if (/^(clear|退出|exit|quit)/gim.test(text)) {
+    gptUserList = gptUserList.filter((user) => user !== contact);
+    await resetMessage(contact.toString());
+    await message.say("退出成功！");
+    return;
+  }
+  if (/^(reset|重置)/gim.test(text)) {
+    await resetMessage(contact.toString());
+    await message.say("重置对话成功！");
+    await asyncSleep(1 * 1e3);
+    await message.say("您可以输入新的内容了！");
+    return;
+  }
+  if (/^(usage|额度|用量)/gim.test(text)) {
+    const humanUsage = await messageManager.getUsagePrint(contact.toString());
+    console.log(humanUsage)
+    await message.say(humanUsage!);
+    return
+  }
+  if (gptUserList.includes(contact) && text) {
+    console.log(
+      `${contact} call gpt api @${new Date().toLocaleString()} with text: ${text}`
+    );
+    let reply = await sendMessage(text, contact.toString());
+    if (/\[errored\]$/gim.test(reply)) {
+      reply = "遇到问题了，请稍后再试，或输入 重置 试试！";
+      console.log(reply);
+    }
+    if (/\[context_length_exceeded\]$/gim.test(reply)) {
+      reply = "本轮会话长度太长啦，我记不住这么多东西，抱歉！请输入 重置 试试！";
+      console.log(reply);
+    }
+    await message.say(reply);
+  }
+}
+
+const listeners = [onScan, onLogout, onLogin, onFriendship, onMessage];
+export const bindListeners = (bot: WechatyInterface) => {
+  return bot
+    .on("scan", onScan)
+    .on("login", onLogin)
+    .on("logout", onLogout)
+    .on("message", (message: MessageInterface) => {
+      onMessage(message, bot)
+    })
+    .on("friendship", (friendship: FriendshipInterface) =>
+      onFriendship(friendship, bot)
+    );
+};
