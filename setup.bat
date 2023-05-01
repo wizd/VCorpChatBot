6abb79f7e3d1c0e58cf7b21554e06d48795c06bb
wizard <wizard@DESKTOP-2LOR8VV>
2023-05-01 18:45:25 +0800

    update setup

--- setup.bat
+++ setup.bat
@@ -1,5 +1,10 @@
 npm install -g typescript ts-node
 npm install
-copy .env.example .env
-notepad .env
+if exist .env (
+    echo .env 文件已经存在，不需要拷贝
+) else (
+    copy .env.example .env
+    notepad .env
+)
+
 
