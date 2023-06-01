5f01354ddf393c907fb6fad20a5e8baea40618e9
wizard <wizard@DESKTOP-2LOR8VV>
2023-06-01 10:53:10 -0700

    support padlocal

--- runx.bat
+++ runx.bat
@@ -10,11 +10,14 @@ set NAME="Some great name"
 rem 下面是你从微可AI引擎得到的API密钥，通常以bk_打头：
 set VCORP_AI_KEY=bk_......
 
+rem 如果是powerbot模式，必须有padlocal的key
+set PAD_LOCAL_KEY=puppet_padlocal_...
+
 rem 下面服务器地址默认不用改
 set VCORP_AI_URL=https://mars.vcorp.ai/vc/v1
 
 :loop
-ts-node --esm index.ts --MODE %MODE% --VCORP_AI_URL %VCORP_AI_URL% --NAME %NAME% --VCORP_AI_KEY %VCORP_AI_KEY%
+ts-node --esm index.ts --MODE %MODE% --VCORP_AI_URL %VCORP_AI_URL% --NAME %NAME% --VCORP_AI_KEY %VCORP_AI_KEY% --PAD_LOCAL_KEY %PAD_LOCAL_KEY%
 if %errorlevel% neq 0 (
     echo Process exited with code %errorlevel%. Waiting for 30 seconds before restarting...
     timeout /t 30 /nobreak
