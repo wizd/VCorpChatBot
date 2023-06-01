5aec574ea750ce9e91874e1a762f4a7b19c6df7b
wizard <wizard@DESKTOP-2LOR8VV>
2023-06-01 08:01:21 -0700

    support retry connect in runx

--- runx.bat
+++ runx.bat
@@ -1,4 +1,22 @@
+@echo off
 chcp 65001
 
-ts-node --esm index.ts --MODE personal --VCORP_AI_URL https://mars.vcorp.ai/vc/v1 --NAME "Some great name" --VCORP_AI_KEY bk_......
+rem 下面是运行模式：配合padlocal时请用powerbot，否则请用personal
+set MODE=personal
 
+rem 下面是运行窗口标题名字：自己觉得清晰可记就行。
+set NAME="Some great name"
+
+rem 下面是你从微可AI引擎得到的API密钥，通常以bk_打头：
+set VCORP_AI_KEY=bk_......
+
+rem 下面服务器地址默认不用改
+set VCORP_AI_URL=https://mars.vcorp.ai/vc/v1
+
+:loop
+ts-node --esm index.ts --MODE %MODE% --VCORP_AI_URL %VCORP_AI_URL% --NAME %NAME% --VCORP_AI_KEY %VCORP_AI_KEY%
+if %errorlevel% neq 0 (
+    echo Process exited with code %errorlevel%. Waiting for 30 seconds before restarting...
+    timeout /t 30 /nobreak
+    goto loop
+)
